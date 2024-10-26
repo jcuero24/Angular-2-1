57038e86714b4e3dc3def6127b5eceac6502a049
Juan Cuero <cueroj505@gmail.com>
2024-10-25 21:05:26 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./components/layout/menu/menu.component";
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { SignalEjemplo1Component } from './components/signal-ejemplo1/signal-ejemplo1.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            MenuComponent, 
+            FooterComponent,
+            SignalEjemplo1Component
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'primeng1';
+export class AppComponent  {
+ 
 }
